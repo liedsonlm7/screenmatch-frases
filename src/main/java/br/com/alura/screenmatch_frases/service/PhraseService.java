60a4b262bd64a1f764aea73d682f603bd40df9a3
liedsonlm7 <liedsonlm7@gmail.com>
2025-09-18 11:27:25 -0300

    config cors

--- src/main/java/br/com/alura/screenmatch_frases/service/PhraseService.java
+++ src/main/java/br/com/alura/screenmatch_frases/service/PhraseService.java
@@ -1,9 +1,18 @@
 package br.com.alura.screenmatch_frases.service;
 
 import br.com.alura.screenmatch_frases.dto.PhraseDTO;
+import br.com.alura.screenmatch_frases.model.Phrase;
+import br.com.alura.screenmatch_frases.repository.PhraseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PhraseService {
-    public PhraseDTO getRandomPhrase() {
+    @Autowired
+    private PhraseRepository repository;
 
+    public PhraseDTO getRandomPhrase() {
+        Phrase phrase = repository.findById(4L).get();
+        return new PhraseDTO(phrase.getTitulo(), phrase.getFrase(), phrase.getPersonagem(), phrase.getPoster());
     }
 }
